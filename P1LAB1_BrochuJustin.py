533f7db8fd8f33fff220882611f8956a866d2b73
Justin Brochu <brochuj1073@student.faytechcc.edu>
2025-09-15 23:42:24 -0400

    Add files via upload

---
+++ P1LAB1_BrochuJustin.py
@@ -0,0 +1,10 @@
+# Justin Brochu
+# 9/15/25
+# P1LAB1
+# Being able to type a first and last name.
+  
+
+
+First_Name=input("Enter your first name:")
+Last_Name=input("Enter your last name:")
+print("Hello,", First_Name, Last_Name, "! Welcome to CTI-110")
