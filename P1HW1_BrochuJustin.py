4f5388844309cefcc75bb2ff346cf4332c58c293
Justin Brochu <brochuj1073@student.faytechcc.edu>
2025-09-25 19:32:25 -0400

    Add files via upload

---
+++ P1HW1_BrochuJustin.py
@@ -0,0 +1,35 @@
+# Justin Brochu
+# 9/25/25
+# P1HW1
+# Using Mathematical Expressions
+
+
+print("-----Calculating Exponenets-----")
+print()
+print()
+print()
+print("Enter an integer as the base value:", end="")
+basevalue = int(input()) 
+print("Enter an integer as the exponent:", end="")
+exponent = int(input())
+print()
+print()
+outcome = basevalue ** exponent
+print(basevalue, "raised to the power of", exponent, "is", outcome, "!!")
+print()
+print()
+print()
+print("-----Addition and Subtraction-----")
+print()
+print()
+print()
+print("Enter a starting integer:", end="")
+starting = int(input())
+print("Enter an integer to add:", end="")
+add = int(input())
+print("Enter an integer to subtract:", end="")
+subtract = int(input())
+print()
+print()
+outcome = starting + add - subtract
+print(starting, "+", add, "-", subtract, "is equal to", outcome)
