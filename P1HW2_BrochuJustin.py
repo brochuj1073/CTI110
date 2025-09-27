1a4058405a048e602c65cc9832b64e89372e430c
Justin Brochu <brochuj1073@student.faytechcc.edu>
2025-09-27 19:50:06 -0400

    Add files via upload

---
+++ P1HW2_BrochuJustin.py
@@ -0,0 +1,42 @@
+# Justin Brochu
+# 9/27/25
+# P1HW2
+# Budget
+# program Pseudocode (logic) 
+
+
+
+
+
+print("This program calculates and displays travel expenses")
+print()
+print("Enter Budget:", end="")
+Budget= int(input())
+print()
+print("Enter your travel destination:", end=""); (input())
+print()
+print("How much do you think you will spend on gas?", end="")
+gas= int(input())
+print()
+print("Approximately, how much will you need for accomodation/hotel?", end="")
+hotel= int(input())
+print()
+print("Last, how much do you need for food?", end=""), 
+food= int(input())
+print()
+print("------------Travel Expenses------------")
+
+print("Location:", end=""); (input())
+print("Initial Budget:", end="")
+InitialBudget= int(input())
+print()
+print("Fuel:", end="")
+Fuel= int(input())
+print("Accomodation:", end="") 
+Accomodation= int(input())
+print("Food:", end="")
+Food= int(input())
+outcome= Fuel + Accomodation + Food 
+remaining= InitialBudget - outcome
+print()
+print("Remaining Balance:", remaining )
